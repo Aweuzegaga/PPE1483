94760311e7b129a554fe8fd263f4e0ef1e157a66
Aweuzegaga <mathieu.yahiko@hotmail.fr>
2015-02-11 11:40:56 +0100

    premier commit

---
+++ app/Controller/PPEController.php
@@ -0,0 +1,26 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PPEController
+ *
+ * @author Philippe
+ */
+class PPEController {
+       /**
+     * index method : first page
+     *
+     * @return void
+     */
+    public function index()
+    {
+        die('test');
+    }
+}
